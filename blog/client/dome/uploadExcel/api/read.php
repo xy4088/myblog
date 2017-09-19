730db7573b99099eac49030fa33843e6333e7e8d
xy <xy@zdongpin.com>
2017-09-19 17:53:39 +0800

    ...

--- blog/client/dome/uploadExcel/api/read.php
+++ blog/client/dome/uploadExcel/api/read.php
@@ -10,7 +10,7 @@ class ExcelToArrary extends Service{
     public function __construct() {
 
         /*导入phpExcel核心类    注意 ：你的路径跟我不一样就不能直接复制*/
-        include_once('./Excel/PHPExcel.php');
+        include_once('../Classes/PHPExcel.php');
     }
 
     /**
