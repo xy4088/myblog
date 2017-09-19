7f3ff534d9aaddc7c16d7ac5a20c964f9fdee57b
xy <xy@zdongpin.com>
2017-09-19 17:34:39 +0800

    ...

--- blog/client/dome/uploadExcel/api/upload.php
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -10,9 +10,9 @@ header("Content-Type:text/html;charset=utf-8");;
 include '../Classes/PHPExcel.php';
 include '../Classes/PHPExcel/IOFactory.php';
 //echo(empty ($_FILES ['file_stu'] ['name']));
+echo($_FILES);
 $fileName = $_FILES['xlfile']['name'];
-echo($_FILES ['xlfile']['name']);
-echo(empty($fileName));
+$file_types = explode ( ".", $fileName );
 if($_FILES ['xlfile']['name']){ //上传文件，成功返回true
     echo '上传成功';
 } else {
