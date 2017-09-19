bfff0654c5863c71babdd660a847495f7511f2a2
xy <xy@zdongpin.com>
2017-09-19 17:53:14 +0800

    ...

---
+++ blog/client/dome/uploadExcel/api/read.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/19 0019
+ * Time: 下午 17:52
+ */
+class ExcelToArrary extends Service{
+
+    public function __construct() {
+
+        /*导入phpExcel核心类    注意 ：你的路径跟我不一样就不能直接复制*/
+        include_once('./Excel/PHPExcel.php');
+    }
+
+    /**
+
+     * 读取excel $filename 路径文件名 $encode 返回数据的编码 默认为utf8
+
+     *以下基本都不要修改
+
+     */
+
+    public function read($filename,$encode='utf-8'){
+
+        $objReader = PHPExcel_IOFactory::createReader('Excel5');
+
+        $objReader->setReadDataOnly(true);
+
+        $objPHPExcel = $objReader->load($filename);
+
+        $objWorksheet = $objPHPExcel->getActiveSheet();
+
+        $highestRow = $objWorksheet->getHighestRow();
+        $highestColumn = $objWorksheet->getHighestColumn();
+        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+        $excelData = array();
+        for ($row = 1; $row <= $highestRow; $row++) {
+            for ($col = 0; $col < $highestColumnIndex; $col++) {
+                $excelData[$row][] =(string)$objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
+            }
+        }
+        return $excelData;
+
+    }
+
+}
