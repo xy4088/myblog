83e13e7e3c02e382ce638ef6ce9f2ad75fc371f9
xy <xy@zdongpin.com>
2017-09-19 11:32:21 +0800

    ...

--- blog/client/dome/uploadExcel/api/upload.php
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -8,7 +8,7 @@
 
 header("Content-Type:text/html;charset=utf-8");
 echo $_FILES['file'];
-//print_r($_FILES);
+print_r($_FILES);
 if(move_uploaded_file($_FILES['file']['tmp_name'],'./uploads/up.xls')){ //上传文件，成功返回true
     echo '上传成功';
 } else {
