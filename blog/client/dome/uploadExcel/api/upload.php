334fec68fd797611f85c1142e6b7cd2f83fc076e
xy <xy@zdongpin.com>
2017-09-19 17:40:08 +0800

    ...

--- blog/client/dome/uploadExcel/api/upload.php
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -12,13 +12,66 @@ include '../Classes/PHPExcel/IOFactory.php';
 //echo(empty ($_FILES ['file_stu'] ['name']));
 echo $_FILES;
 $fileName = $_FILES['xlfile']['name'];
-$file_types = explode ( ".", $fileName );
-$file_type = $file_types[count( $file_types ) - 1];
-echo json_encode($file_types);
-echo json_encode('      ----       ');
-echo json_encode($file_type);
 if($_FILES ['xlfile']['name']){ //上传文件，成功返回true
-    echo '上传成功';
+    $file_types = explode ( ".", $fileName );
+    $file_type = $file_types[count( $file_types ) - 1];
+    /*判别是不是.xls文件，判别是不是excel文件*/
+    if (strtolower ( $file_type ) != "xls")
+    {
+        $this->error ( '不是Excel文件，重新上传' );
+    }
+
+    /*设置上传路径*/
+    $savePath = SITE_PATH . '/public/upfile/Excel/';
+
+    /*以时间来命名上传的文件*/
+    $str = date ( 'Ymdhis' );
+    $file_name = $str . "." . $file_type;
+
+    /*是否上传成功*/
+    if (! copy ( $tmp_file, $savePath . $file_name ))
+    {
+        $this->error ( '上传失败' );
+    }
+
+    /*
+
+       *对上传的Excel数据进行处理生成编程数据,这个函数会在下面第三步的ExcelToArray类中
+
+      注意：这里调用执行了第三步类里面的read函数，把Excel转化为数组并返回给$res,再进行数据库写入
+
+    */
+    $res = Service ( 'ExcelToArray' )->read ( $savePath . $file_name );
+
+    /*
+
+         重要代码 解决Thinkphp M、D方法不能调用的问题
+
+         如果在thinkphp中遇到M 、D方法失效时就加入下面一句代码
+
+     */
+    //spl_autoload_register ( array ('Think', 'autoload' ) );
+
+    /*对生成的数组进行数据库的写入*/
+    foreach ( $res as $k => $v )
+    {
+        if ($k != 0)
+        {
+            $data ['uid'] = $v [0];
+            $data ['password'] = sha1 ( '111111' );
+            $data ['email'] = $v [1];
+
+            $data ['uname'] = $v [3];
+
+            $data ['institute'] = $v [4];
+            $result = M ( 'user' )->add ( $data );
+            if (! $result)
+            {
+                $this->error ( '导入数据库失败' );
+            }
+        }
+    }
+
 } else {
     echo '上传失败';
 }
