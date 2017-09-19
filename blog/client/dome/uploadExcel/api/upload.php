bcc480758cde2e4a1bc109407d75f893e927541d
xy <xy@zdongpin.com>
2017-09-19 11:51:01 +0800

    ...

--- blog/client/dome/uploadExcel/api/upload.php
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -6,15 +6,11 @@
  * Time: 上午 11:24
  */
 
-header("Content-Type:text/html;charset=utf-8");
-//echo $_FILES['file'];
-//print_r($_FILES);
-//print_r($_FILES['file']);
-echo(empty ($_FILES ['file_stu'] ['name']));
+header("Content-Type:text/html;charset=utf-8");;
+//echo(empty ($_FILES ['file_stu'] ['name']));
 echo($_FILES ['file_stu'] ['tmp_name']);
-echo(explode ( ".", $_FILES ['file_stu'] ['name'] ));
-echo($file_types [count ( $file_types ) - 1]);
-//echo(json_encode($_FILES));
+//echo(explode ( ".", $_FILES ['file_stu'] ['name'] ));
+//echo($file_types [count ( $file_types ) - 1]);
 if(move_uploaded_file($_FILES['file']['tmp_name'],'./uploads/up.xls')){ //上传文件，成功返回true
 //    echo '上传成功';
 } else {
