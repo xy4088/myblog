ff7f385a77b809d98d50ef97f3927471b4010730
xy <xy@zdongpin.com>
2017-09-19 11:25:30 +0800

    ...

---
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/19 0019
+ * Time: 上午 11:24
+ */
+
+header("Content-Type:text/html;charset=utf-8");
+echo $_FILES['file'];
+//print_r($_FILES);
+if(move_uploaded_file($_FILES['file']['tmp_name'],'./uploads/up.xls')){ //上传文件，成功返回true
+    echo '上传成功';
+} else {
+    echo '上传失败';
+}
--- blog/client/dome/uploadExcel/index.php
+++ blog/client/dome/uploadExcel/index.php
@@ -5,14 +5,5 @@
  * Date: 2016/6/14
  * Time: 15:54
  */
-header("Content-Type:text/html;charset=utf-8");
-echo $_FILES['file'];
-//print_r($_FILES);
-if(move_uploaded_file($_FILES['file']['tmp_name'],'./uploads/up.xls')){ //上传文件，成功返回true
-    echo '上传成功';
-} else {
-    echo '上传失败';
-}
-
 include_once 'excel.html';
 ?>
