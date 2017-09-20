a1f49028823cb6011b4bb68cfb53c11e8f6cd591
xy <xy@zdongpin.com>
2017-09-20 14:04:24 +0800

    ...

--- blog/client/dome/uploadExcel/api/upload.php
+++ blog/client/dome/uploadExcel/api/upload.php
@@ -10,6 +10,7 @@ header("Content-Type:text/html;charset=utf-8");
 //include '../Classes/PHPExcel.php';
 //include '../Classes/PHPExcel/IOFactory.php';
 include 'read.php';
+echo $_FILES['xlfile'];
 echo empty ($_FILES['xlfile']);
 //$fileName = $_FILES['xlfile']['tmp_name'];
 echo $_FILES['xlfile'];
